66d4190cc07ca906cad0ce6a7657b95e9d4e7f5c
Nerada <5610242+Nerada@users.noreply.github.com>
2022-08-05 15:09:04 +0200

    Loading of the seed

---
+++ GameOfLife/GameOfLife/Extensions/MapExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using GameOfLife.Model;
+
+namespace GameOfLife.Extensions;
+
+public static class MapExtensions
+{
+    public static List<Cell> ToMap(this string pointString)
+    {
+        List<Cell> map = new();
+
+        List<string> pointRows = pointString.Split("\r\n", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        for (int columnIndex = 0; columnIndex < pointRows.Count; columnIndex++)
+        {
+            for (int rowIndex = 0; rowIndex < pointRows[columnIndex].Length; rowIndex++)
+            {
+                map.Add(new Cell(new Point(rowIndex, columnIndex), pointRows[columnIndex][rowIndex].ToState()));
+            }
+        }
+
+        return map;
+    }
+
+    private static Cell.State ToState(this char character) =>
+        character switch
+        {
+            '.' => Cell.State.Dead,
+            '#' => Cell.State.Alive,
+            _   => throw new InvalidOperationException()
+        };
+}
---
+++ GameOfLife/GameOfLife/Model/Cell.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------
+//     Author: Ramon Bollen
+//      File: GameOfLife.Cell.cs
+// Created on: 20220805
+// -----------------------------------------------
+
+using System.Windows;
+
+namespace GameOfLife.Model;
+
+public class Cell
+{
+    public enum State
+    {
+        Alive,
+        Dead
+    }
+
+    public Cell(Point location, State status)
+    {
+        Location = location;
+        Status   = status;
+    }
+
+    public State Status { get; private set; }
+
+    public Point Location { get; }
+
+    public void ChangeStatus(State newStatus) => Status = newStatus;
+}
